a8ed5d50b0499dc77b8468689f6690f0f9554f2d
geazibuceli <geazijunior1996@gmail.com>
2026-09-14 17:26:57 -0300

    chore: remove tracked build artifacts and Python caches

--- build/lib/miniencoder/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Educational sentiment classification with PyTorch encoder models."""
-
-from .baselines import MeanEmbeddingClassifier
-from .model import TransformerClassifier
-
-__all__ = ["MeanEmbeddingClassifier", "TransformerClassifier"]
